387a2f4298069e8d9f12e472c058f7586f28e87a
Atif Pathan <atifkhan.p1@gmail.com>
2024-08-21 16:22:33 -0600

    Initial commit

---
+++ src/main/java/com/atifpathan/gameboxxd/GameboxxdApplication.java
@@ -0,0 +1,13 @@
+package com.atifpathan.gameboxxd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameboxxdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameboxxdApplication.class, args);
+	}
+
+}
